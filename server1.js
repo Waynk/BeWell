5a98121e5c9283b7b8a1618f9192f71320c82bbd
waynk <ww930304h@gmail.com>
2025-07-25 18:14:12 +0800

    更新 Node.js 連線設定與功能

--- server1.js
+++ server1.js
@@ -15,7 +15,7 @@ const jwt = require("jsonwebtoken");
 const axios = require("axios");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
